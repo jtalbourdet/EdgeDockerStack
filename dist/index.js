e20a5f590aecbf3045a4a0844c24a99b2502beca
TALBOURDET <julien.talbourdet@gmail.com>
2020-02-24 11:31:38 +0100

    cleanning

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const testModule_1 = require("./testModule");
-const demo = new testModule_1.default;
-function testFct() {
-    demo.demoMsg();
-}
-setInterval(testFct, 3000);
-//# sourceMappingURL=index.js.map
--- dist/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var msg = "hello wago";
-exports.msg = msg;
-//# sourceMappingURL=test.js.map
