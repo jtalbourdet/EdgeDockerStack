d18e83193ce39830234821fc092feee98c5c7d36
TALBOURDET <julien.talbourdet@gmail.com>
2020-09-23 10:26:29 +0200

    restructuration

--- containers-confs/script/influxdb/init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function createDb {
-    echo " *****Création base de donnée $1 *****"
-    influx -execute "CREATE DATABASE $1"
-}
-
-# Créate new db when container start
-#createDb db_metrics_tests
