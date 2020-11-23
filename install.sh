557c936d05319b6330cb8dab103bef9115b1def0
TALBOURDET <julien.talbourdet@gmail.com>
2020-11-23 22:16:37 +0100

    update install.sh chmod

--- install.sh
+++ install.sh
@@ -104,13 +104,14 @@ echo "* Configure telegraf influxdb connection"
 sed -i "s/username =.*/username = \"$adminUserName\"/" $TELEGRAF_CONF_FILE_PATH 2>> $INSTALL_LOG_FILE_PATH
 sed -i "s/password =.*/password = \"$adminPassword\"/" $TELEGRAF_CONF_FILE_PATH 2>> $INSTALL_LOG_FILE_PATH
 
+
 mkdir containers-datas
 chmod 777 -R containers-datas >> $INSTALL_LOG_FILE_PATH 2>&1
+chmod 777 -R containers-confs >> $INSTALL_LOG_FILE_PATH 2>&1
 
 echo "* Create containers"
 docker-compose -f "docker-compose.yml" up -d --build >> $INSTALL_LOG_FILE_PATH 2>&1
 
-chmod 777 -R containers-datas >> $INSTALL_LOG_FILE_PATH 2>&1
 
 echo "* Installation logs are avaliable in installLogs.log file"
 echo "* You can now publish your MQTT message on the influxdb\/ topic and visualize them in Grafana from the db_metrics database"
